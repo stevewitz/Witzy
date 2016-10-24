915cd8e4cdd5423cdaeb62a4494c36bc2f619681
stevewitz <stevewitz@wizcomputing.com>
2016-10-23 18:34:33 -0700

    rainbow increment works

--- rgbled.js
+++ rgbled.js
@@ -349,7 +349,9 @@ function rainbow(o,value){
     var rainbowArray = [0xFF, 0x00, 0x00, 0xFF, 0x7F, 0x00, 0xFF, 0xFF, 0x00,0x00, 0xFF, 0x00, 0x00, 0x00, 0xFF, 0x4b, 0x00, 0x82, 0x94, 0x00, 0xd3]; //rainbow colors
     var numToIncrement = 0
     for(var i = o.startLed-1; i < o.endLed*3; i+=rainbowArray.length*numEachColor) {//operate on the
-
+        if( (i + 20 +j)>o.endLed *3){
+            break;
+        }
         for (var j = 0; j < rainbowArray.length; j+=3) {
             var countloops = 0;
             while(countloops < numEachColor) {
