34bb01c94d86c893ca812e3c44ed0e5920f96a6d
Todd <twitzel@gmail.com>
2016-11-17 17:40:16 -0700

    Added support for checkboxes

--- traceinverter.js
+++ traceinverter.js
@@ -583,7 +583,7 @@ exports.getInverterValue(4,1,25,function(x){
                                 o.generatorVolts = x.value;
                                 exports.getInverterValue(4,9,5,function(x){
                                     o.freq = x.value;
-                                    o.chargerWatts = o.chargerCurrent*o.batteryVolts;
+                                    o.chargerWatts = o.chargerAmps*o.batteryVolts;
                                     o.inputWatts = o.inputAmps*o.gridVolts;
                                     o.outputWatts = o.outputWatts*o.inverterVolts;
                                     console.log(JSON.stringify(o,null,4))
