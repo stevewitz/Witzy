4e839d7adf4e2c7406b463802e2d0e70dc295120
Todd <twitzel@gmail.com>
2016-11-21 13:49:06 -0700

    xantrex,trace and outback working

--- witzyserver.js
+++ witzyserver.js
@@ -20,8 +20,8 @@ exports.start = function() {
         res.send(500, 'Something broke!');
     });
     app.post('/api', function (req, res) {
-        console.log('here - incomming api command:'+JSON.stringify(req.body,null,4))
-        switch (req.body.obj.device){
+        console.log('here - incomming api command:'+JSON.stringify(req.body.command,null,4))
+        switch (req.body.obj.commands.device){
             case 'rgb':
                 rgb.incommand(req.body);
                 break;
