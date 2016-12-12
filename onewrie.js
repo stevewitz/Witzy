598afe9c1bd760b68e73a7ebc63f04e7f5482d47
Todd <twitzel@gmail.com>
2016-12-12 11:38:42 -0700

    xantrex,trace and outback working

---
+++ onewrie.js
@@ -0,0 +1,24 @@
+/**
+ * Created by todd on 12/12/2016.
+ */
+var sensors = require('1wire');
+
+// promise based
+sensors.readDevices().then(
+    function (devices) {
+        console.log('Read all devices', devices);
+    },
+    function (err) {
+        console.log('An error occurred', err);
+    }
+);
+
+// callback based
+sensors.readDevices(function (err, devices) {
+    if (err) {
+        console.log('An error occurred', err);
+        return;
+    }
+
+    console.log('Read all devices', devices);
+});
