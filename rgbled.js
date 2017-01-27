00662febbe376dc1ddcdd6027d285dbfa1eb5fa9
stevewitz <stevewitz@wizcomputing.com>
2017-01-27 08:52:02 -0800

    colorgradient mirror

--- rgbled.js
+++ rgbled.js
@@ -588,8 +588,8 @@ function colorGradientMirror(o,value) {
         rgbBuffer[o.stripname][i+2+3]= rgbBuffer[o.stripname][i+2]-blueDelta;
     }
     for( var i = parseInt(((o.endLed -1)*3)/2); i < (o.endLed-1)*3; i++){// change the second half
-        //rgbBuffer[o.stripname][i]= rgbBuffer[o.stripname][((o.endLed-1)*3)/2-i]
-        rgbBuffer[o.stripname][i]=255;
+        rgbBuffer[o.stripname][i]= rgbBuffer[o.stripname][((o.endLed-1)*3)/2-i]
+
         console.log(i);
     }
     updatestrip(o, rgbBuffer[o.stripname]);
